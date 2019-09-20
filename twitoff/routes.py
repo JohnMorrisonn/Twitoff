7a6b6ffef6ce8019771a347ece90fd3017866ed4
John Morrison <jjosephmorrison@gmail.com>
2019-09-19 22:45:28 -0400

    Final app w/o CSS fixes

--- twitoff/routes.py
+++ twitoff/routes.py
@@ -1,13 +1,19 @@
 from flask import render_template, request
 from twitoff import db, app
 from .models import User, Tweet
-from .twitter import add_or_update_user
+from .twitter import add_or_update_user, update_all_users, add_users
+from .predict import predict_user
 
 
 @app.route('/')
 def root():
     users = User.query.all()
-    return render_template('base.html', title='Home', users=users)
+    return render_template('base.html', title="Who's It Going to Be?", users=users)
+
+
+@app.route('/bulma')
+def bulma():
+    return render_template('bulma_base.html')
 
 
 @app.route('/user', methods=['POST'])
@@ -27,22 +33,27 @@ def user(name=None):
                            message=message)
 
 
-@app.route('/compare')
+@app.route('/compare', methods=['POST'])
 def compare(message=''):
     user1, user2 = sorted([request.values['user1'],
                           request.values['user2']])
     if user1 == user2:
         message = 'Cannot compare a user to themselves!'
+        return render_template('prediction.html', title='Prediction', message=message)
     else:
         tweet_text = request.values['tweet_text']
         confidence = int(predict_user(user1, user2, tweet_text) * 100)
-
+        if confidence >= 50:
+            message = f'"{tweet_text}" is more likely to be said by {user1} than {user2}, with {confidence}% confidence'
+        else:
+            message = f'"{tweet_text}" is more likely to be said by {user2} than {user1}, with {100-confidence}% confidence'
+        return render_template('prediction.html', title='Prediction', message=message)
 
 @app.route('/update')
 def update():
     update_all_users()
     return render_template('base.html', title='Update all users!', users=User.query.all())
-
+    
 
 @app.route('/reset')
 def reset():
