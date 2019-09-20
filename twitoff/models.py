41a9b1d4c83d0352f17421617435f5a7c98d1ccd
John Morrison <jjosephmorrison@gmail.com>
2019-09-20 16:36:24 -0400

    bigintr

--- twitoff/models.py
+++ twitoff/models.py
@@ -2,7 +2,7 @@ from twitoff import db
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.BigInteger, primary_key=True)
     name = db.Column(db.String(20), unique=True, nullable=False)
     newest_tweet_id = db.Column(db.BigInteger)
 
